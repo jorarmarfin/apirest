639095fd3db350bf9124bc4297e54d93c68feeee
Luis mayta <luis.mayta@gmail.com>
2019-04-23 11:20:14 -0500

    First commit

---
+++ app/Distrito.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Distrito extends Model
+{
+    protected $table = 'distrito';
+    protected $guarded = [];
+    public $timestamps = false;
+}
---
+++ app/Provincium.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Provincium extends Model
+{
+    protected $table = 'provincia';
+    protected $guarded = [];
+    public $timestamps = false;
+}
+ 
